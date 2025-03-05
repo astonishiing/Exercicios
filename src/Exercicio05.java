00ffa3cf1cd467a801ae7ab198ee8c5cfe396d5a
astonishiing <wcardoso4510@gmail.com>
2025-03-05 08:54:54 -0300

    exercicio05

---
+++ src/Exercicio05.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Exercicio05 {
+    public static void main(String[] args) {
+        BufferedReader leitor = new BufferedReader(new InputStreamReader(System.in));
+        int codigoUsuario
+    }
+}
